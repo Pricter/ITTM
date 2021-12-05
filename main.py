a671498c8158bce9d6867d73186201c92153c44d
Pricter <kodithiyalaveekshithv2@gmail.com>
2021-12-05 10:00:47 +0530

    Format the code, and add ability to get tools using version

--- main.py
+++ main.py
@@ -1,47 +1,79 @@
 import json
-from ssl import match_hostname
 import urllib.request
 import os
 import platform
 import errno
 import requests
+import shutil
+from rich.console import Console
+
+console = Console()
 
 
-class colors:
-    reset = '\033[0m'
-    bold = '\033[01m'
-    disable = '\033[02m'
-    underline = '\033[04m'
-    reverse = '\033[07m'
-    strikethrough = '\033[09m'
-    invisible = '\033[08m'
+class codes:
+    err = "-1"
+    warning = "0"
+    key = "1"
+    info = "2"
+    inst = "3"
+    tool = "4"
 
-    class fg:
-        black = '\033[30m'
-        red = '\033[31m'
-        green = '\033[32m'
-        orange = '\033[33m'
-        blue = '\033[34m'
-        purple = '\033[35m'
-        cyan = '\033[36m'
-        lightgrey = '\033[37m'
-        darkgrey = '\033[90m'
-        lightred = '\033[91m'
-        lightgreen = '\033[92m'
-        yellow = '\033[93m'
-        lightblue = '\033[94m'
-        pink = '\033[95m'
-        lightcyan = '\033[96m'
 
-    class bg:
-        black = '\033[40m'
-        red = '\033[41m'
-        green = '\033[42m'
-        orange = '\033[43m'
-        blue = '\033[44m'
-        purple = '\033[45m'
-        cyan = '\033[46m'
-        lightgrey = '\033[47m'
+def cprint(text, code):
+    if(code == codes.err):
+        console.print(f"[ ERROR ]: {text}\n", style="red")
+    if(code == codes.key):
+        console.print(f"[ KEY ]: {text}\n", style="green")
+    if(code == codes.info):
+        console.print(f"[ INFO ]: {text}\n", style="blue")
+    if(code == codes.inst):
+        console.print(f"\t[ INSTALL ]: {text}\n", style="yellow")
+    if(code == codes.tool):
+        console.print(f"\n\t[ TOOLS ]: {text}\n", style="magenta")
+
+
+def log(log, cmd='null', aList=[]):
+    if(log) == "nCmd":
+        cprint("No command provided, Quitting the program", codes.err)
+    elif(log) == "gFile":
+        cprint("Getting release file, this may take some time", codes.inst)
+    elif(log) == "tArgs":
+        cprint(f"Too many arguments for `{cmd}` command", codes.err)
+    elif(log) == "availT":
+        cprint("Available Tools:", codes.info)
+    elif(log) == "nToolsIns":
+        cprint("No tools currently installed", codes.info)
+    elif(log) == "availInsTools":
+        cprint("\nCurrent installed tool list:", codes.info)
+    elif(log) == "unknownArg":
+        cprint(f"Unknown argument `{aList[0]}`", codes.err)
+    elif(log) == "nArgs":
+        cprint("Not enough arguments for `{cmd}` command", codes.err)
+    elif(log) == "tAIns":
+        cprint("`{aList[0]}` already installed", codes.err)
+    elif(log) == "toolNotFoundMain":
+        cprint(
+            f"`{aList[0]}` was not found in the main list of tools", codes.err)
+    elif(log) == "permD":
+        cprint(
+            "Permission denied run ittm using sudo or as administrator", codes.err)
+    elif(log) == "missingTool":
+        cprint(
+            f"`{aList[0]}` seems to be installed but could not find it in `toollist.txt`", codes.err)
+    elif(log) == "vGetError":
+        cprint(
+            "Version does not exist or server may be down", codes.err)
+    elif(log) == "unknownDeleteError":
+        cprint(
+            "Could not delete directory upon failure", codes.err)
+    elif(log) == "nEnoughSArgs":
+        cprint("Invalid argument or not enough sub arguments", codes.err)
+    elif(log) == "unknownCmd":
+        cprint("Unknown command, Please use valid commands", codes.err)
+    elif(log) == "key":
+        cprint("You are pressing a key combination. Using key combinations for terminal may result in unwanted results", codes.key)
+    else:
+        cprint(f"INTERNAL ERROR, UNKNOWN CODE: {cmd}", codes.err)
 
 
 toollistPath = ""
@@ -75,8 +107,7 @@ def getInput():
     UserData = input("> ")
     splitted = UserData.split()
     if(len(splitted) < 1):
-        print(
-            f"\n{colors.fg.red}[ERROR]: No command provided, Quitting the program{colors.reset}\n")
+        log("ncmd")
         exit()
     command = splitted[0]
     splitted.pop(0)
@@ -85,8 +116,7 @@ def getInput():
 
 
 def getFile(url, path):
-    print(
-        f"\t{colors.fg.yellow}[ INSTALL ]: Getting release file, this may take some time.{colors.reset}\n")
+    log("gFile")
     file_name = url.split('/')[-1]
     fp = os.path.join(path, file_name)
     urllib.request.urlretrieve(url, fp)
@@ -97,26 +127,22 @@ def parse(command, args):
         if(len(args) == 0):
             exit()
         elif(len(args) > 0):
-            print(
-                f"\n{colors.fg.red}[ ERROR ]: Too many arguments for `exit` command.{colors.reset}")
+            log("tArgs", "exit")
     elif(command == "listTools"):
         if(len(args) > 1):
-            print(
-                f"\n{colors.fg.red}[ ERROR ]: Too many arguments for `listTools` command.{colors.reset}")
+            log("tArgs", "listTools")
             return None
         if(len(args) == 0):
             tools = [x for x in getTools()]
-            print("\n\t[ TOOLS ]: Available Tools:")
+            log("availT")
             for i in range(len(tools)):
                 print(f"\t\t{tools[i]}")
         if(len(args) == 1):
             if(args[0] == "-i" or "-installed"):
                 if(os.path.isfile(toollistPath) == False):
-                    print(
-                        f"\n{colors.fg.blue}[ INFO ]: No tools currently installed.{colors.reset}")
+                    log("nToolsIns")
                 else:
-                    print(
-                        f"\n{colors.fg.blue}[ INFO ]: Current installed tool list:{colors.reset}")
+                    log("availInsTools")
                     with open(toollistPath, "r") as f:
                         raw = f.readlines()
                         for i in range(len(raw)):
@@ -124,16 +150,13 @@ def parse(command, args):
                     f.close()
                     print()
             else:
-                print(
-                    f"\n{colors.fg.red}[ ERROR ]: Unknown argument `{args[0]}`.{colors.reset}")
+                log("unknownArg", aList=args)
     elif(command == "install"):
-        if(len(args) > 1):
-            print(
-                f"\n{colors.fg.red}[ ERROR ]: Too many arguments for `install` command.{colors.reset}")
+        if(len(args) > 3):
+            log("tArgs", "install")
             return None
         elif(len(args) < 1):
-            print(
-                f"\n{colors.fg.red}[ ERROR ]: Not enough arguments `install` command.{colors.reset}")
+            log("nArgs", "install")
             return None
         raw = []
         if(os.path.isfile(toollistPath) == True):
@@ -141,8 +164,7 @@ def parse(command, args):
                 raw = f.readlines()
                 for i in range(len(raw)):
                     if(raw[i] == args[0]):
-                        print(
-                            f"\n{colors.fg.red}[ ERROR ]: `{args[0]}` already installed.{colors.reset}")
+
                         return None
         raw = getTools()
         tools = [x for x in getTools()]
@@ -151,8 +173,7 @@ def parse(command, args):
             if tools[i] == args[0]:
                 isThere = True
         if isThere == False:
-            print(
-                f"\n{colors.fg.red}[ ERROR ]: `{args[0]}` was not found in the main list of tools.{colors.reset}")
+            log("toolNotFoundMain", aList=args)
             return None
         try:
             os.mkdir(mainPath)
@@ -160,26 +181,69 @@ def parse(command, args):
             if exc == errno.EEXIST:
                 pass
             elif exc == errno.EACCES or errno.EPERM:
-                print(
-                    f"{colors.fg.red}[ ERROR ]: Permission denied run ittm using sudo.{colors.reset}")
+                log("permD")
                 return None
         try:
             os.mkdir(os.path.join(mainPath, args[0]))
         except OSError as exc:
             if exc == errno.EEXIST:
-                print(
-                    f"\n{colors.fg.red}[ ERROR ]: `{args[0]}` seems to be installed but could not find it in `toollist.txt`.{colors.reset}")
+                log("missingTool", aList=args)
                 return None
             elif exc == errno.EACCES or errno.EPERM:
-                print(
-                    f"{colors.fg.red}[ ERROR ]: Permission denied run ittm using sudo.{colors.reset}")
+                log("permD")
                 return None
         if system == "windows":
-            getFile(
-                f"{raw[args[0]]['git']}/releases/tag/{raw[args[0]]['version']}/{raw[args[0]]['file']['Windows']}", os.path.join(mainPath, args[0]))
+            if(len(args) == 1):
+                getFile(
+                    f"{raw[args[0]]['git']}/releases/tag/{raw[args[0]]['version']}/{raw[args[0]]['file']['Windows']}", os.path.join(mainPath, args[0]))
+            elif(len(args) == 3):
+                if args[1] == "-v" or "-version":
+                    response = requests.get(
+                        f"{raw[args[0]]['git']}/releases/tag/{args[2]}/{raw[args[0]]['file']['Windows']}", os.path.join(mainPath, args[0]))
+                    if response.status_code == 200:
+                        pass
+                    else:
+                        log("vGetError")
+                        try:
+                            shutil.rmtree(mainPath)
+                        except:
+                            log("unknownDeleteError")
+                        return None
+                    getFile(
+                        f"{raw[args[0]]['git']}/releases/tag/{args[2]}/{raw[args[0]]['file']['Windows']}", os.path.join(mainPath, args[0]))
+            else:
+                log("nEnoughSArgs")
+                try:
+                    shutil.rmtree(mainPath)
+                except:
+                    log("unknownDeleteError")
+                return None
         if system == "linux":
-            getFile(
-                f"{raw[args[0]]['git']}/releases/tag/{raw[args[0]]['version']}/{raw[args[0]]['file']['Linux']}", os.path.join(mainPath, args[0]))
+            if(len(args) == 1):
+                getFile(
+                    f"{raw[args[0]]['git']}/releases/tag/{raw[args[0]]['version']}/{raw[args[0]['version']]['file']['Linux']}", os.path.join(mainPath, args[0]))
+            elif(len(args) == 3):
+                if args[1] == "-v" or "-version":
+                    response = requests.get(
+                        f"{raw[args[0]]['git']}/releases/tag/{args[2]}/{raw[args[0]]['file']['Linux']}", os.path.join(mainPath, args[0]))
+                    if response.status_code == 200:
+                        pass
+                    else:
+                        log("vGetError")
+                        try:
+                            shutil.rmtree(mainPath)
+                        except:
+                            log("unknownDeleteError")
+                        return None
+                    getFile(
+                        f"{raw[args[0]]['git']}/releases/tag/{args[2]}/{raw[args[0]]['file']['Linux']}", os.path.join(mainPath, args[0]))
+            else:
+                log("nEnoughSArgs")
+                try:
+                    shutil.rmtree(mainPath)
+                except:
+                    log("unknownDeleteError")
+                return None
         try:
             f = open(toollistPath, "x")
             f.close()
@@ -192,8 +256,7 @@ def parse(command, args):
     elif(command == "clear"):
         os.system('cls' if os.name == 'nt' else 'clear')
     else:
-        print(
-            f"\n{colors.fg.red}[ ERROR ]: Unknown command, Please use valid commands.{colors.reset}")
+        log("unknownCmd")
 
 
 try:
@@ -201,5 +264,4 @@ try:
         inputU = getInput()
         parse(inputU[0], inputU[1])
 except KeyboardInterrupt:
-    print(
-        f"\n{colors.fg.green}[ KEY ]: You are pressing a key combination. The proper way to quit the program is using `exit`{colors.reset}\n")
+    log("key")
