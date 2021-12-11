bb1f0efda3231fe9e06c2d9a6769ec00c497cadf
Veekshith Kodithiyala <kodithiyalaveekshithv2@gmail.com>
2021-12-11 14:49:09 +0530

    added mac support

--- src/OsUtil.py
+++ src/OsUtil.py
@@ -11,17 +11,13 @@ if platform.system() == "Windows":
 elif platform.system() == "Linux":
     system = "linux"
 elif platform.system() == "Darwin":
-    system = "mac"
+    system = "linux"
 
 if(system == "windows"):
     toollistPath = "C:\\toolbox\\toollist.txt"
     mainPath = "C:\\toolbox\\"
     toollistcache = "C:\\toolbox\\tool_list_cache.txt"
 elif(system == "linux"):
-    toollistPath = f"{os.path.expanduser('~')}/.toolboxlauncher/toollist.json"
-    mainPath = f"{os.path.expanduser('~')}/.toolboxlauncher/"
-    toollistcache = f"{os.path.expanduser('~')}/.toolboxlauncher/tool_list_cache.json"
-elif(system == "mac"):
     toollistPath = f"{os.path.expanduser('~')}/.toolboxlauncher/toollist.json"
     mainPath = f"{os.path.expanduser('~')}/.toolboxlauncher/"
     toollistcache = f"{os.path.expanduser('~')}/.toolboxlauncher/tool_list_cache.json"
